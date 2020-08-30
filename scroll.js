0ff69731ccf35aa1e549bf2a04933e2fc503c0b6
Ancorale <ancorale@ancorale.com>
2020-08-29 22:23:36 -0700

    8.29.20..2223

---
+++ scroll.js
@@ -0,0 +1,46 @@
+
+const scroll =
+  window.requestAnimationFrame ||
+  // IE Fallback
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+
+  const elementsToShow = document.querySelectorAll(".scroll");
+
+  function loop() {
+    elementsToShow.forEach((e) => {
+      if (
+        isElementInViewport(e) &&
+        !window.matchMedia("(max-width: 480px)").matches
+      ) {
+        e.classList.add("isVisible");
+      } else {
+        e.classList.remove("isVisible");
+      }
+    });
+
+    scroll(loop);
+  }
+
+  // Call the loop for the first time
+  loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
